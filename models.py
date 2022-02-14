93799262794811b30713503297089d2f0d79d291
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-02-14 13:12:50 -0600

    Delete models.py

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-    first_name = models.CharField(max_length = 20)
-    last_name = models.CharField(max_length = 20)
-    CWID = models.CharField(max_length = 9)
