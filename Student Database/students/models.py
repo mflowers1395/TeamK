78d6312b3d2ef6247270441a765039821e1fb2d4
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-02-22 13:23:30 -0600

    Modified user database & html pages
    Made some modifications to how users are authenticated and added to the database to make things a little simpler. Also created some login/logout HTML pages (subject to further modification).

--- Student Database/students/models.py
+++ Student Database/students/models.py
@@ -1,23 +1,3 @@
 from django.db import models
 
-class User(models.Model):
-    firstname = models.CharField(max_length=20)
-    lastname = models.CharField(max_length=20)
-    email = models.EmailField()
-    username = models.CharField(max_length=20)
-    CWID = models.CharField(max_length=9)
-    USERNAME_FIELD = "username"
-
-    def __str__(self):
-        return self.firstname + " " + self.lastname
-
-
-class Textbook(models.Model):
-    booktitle = models.CharField(max_length=30)
-    author = models.CharField(max_length=30)
-    ISBN = models.IntegerField()
-
-    def __str__(self):
-        return self.booktitle
-
-
+# Create your models here.
--- Student Database/students/views.py
+++ Student Database/students/views.py
@@ -1,25 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from users.models import User
-from users.forms import UserRegisterForm
+from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            user = User(firstname=form.cleaned_data['firstname'],lastname=form.cleaned_data['lastname'],email=form.cleaned_data['email'],username=form.cleaned_data['username'],CWID=form.cleaned_data['CWID'])
-            user.save()
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')  # edit redirect to somewhere else if necessary
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect ('login')
+
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
 def userAcct(request):
     
     return render(request, 'users/home.html')
-
-
-    
 
 
