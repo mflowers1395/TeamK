45e20bd53629eb5c56432c09ed9ee6a5c51ec673
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-02-26 12:21:32 -0600

    Basic seller interface
    A basic seller interface. Only the upload textbook functionality is currently working. Still a work-in-progress. Modify python and HTML files as needed

---
+++ Seller/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class UploadTextbookForm(forms.Form):
+
+    title = forms.CharField(max_length=50)
+    author = forms.CharField(max_length=20)
+    isbn = forms.CharField(max_length=13)
+
+
+    
+        
