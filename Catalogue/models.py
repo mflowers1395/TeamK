a28b6d64e468ecf2dd2d99bf0951b47d8633f997
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-02-27 20:05:53 -0600

    Python files for catalogue functionality

---
+++ Catalogue/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Textbook(models.Model):
+    booktitle = models.CharField('Title', max_length=50)
+    author = models.CharField('Author', max_length=30)
+    isbn = models.CharField('ISBN', max_length=13)
+
+    def __str__(self):
+        return self.booktitle
