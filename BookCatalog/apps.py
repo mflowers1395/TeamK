af1a6f0fa81bd250c6169c9a4ff1032aaf560e84
Marilyn Elaine Flowers <60019181+mflowers1395@users.noreply.github.com>
2022-02-27 19:54:13 -0600

    Delete BookCatalog directory

--- BookCatalog/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BookcatalogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BookCatalog'
--- BookCatalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns =[
-    path("mainCatalog/", views.launchBookCatalog),
-    path("searchResults/", views.launchSearchResults),
-    path("buyerPage/", views.launchBuyerPage),
-    path("sellerPage/", views.launchSellerPage),
-]
