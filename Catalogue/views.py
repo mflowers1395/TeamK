fdeb2e82089214b5d87f98471e303a072afa1884
Chris Hunter <56775316+chunter3@users.noreply.github.com>
2022-03-05 17:57:04 -0600

    Updated views.py file
    Updated views.py file to include increased functionality for the catalog search. Search by author and ISBN is now possible

--- Catalogue/views.py
+++ Catalogue/views.py
@@ -1,10 +1,23 @@
 import imp
+from turtle import title
 from django.shortcuts import render
-from Catalogue.models import Textbook
+from catalogue.models import Textbook
 
 def catalogueInterface(request):
+    
     return render(request, 'catalogue/interface.html')
 
 def browseTextbooks(request):
+
     textbook_list = Textbook.objects.all()
+
     return render(request, 'catalogue/browse.html', {'textbook_list': textbook_list})
+
+def searchCatalogue(request):
+
+    if request.method == 'POST':
+
+        searchresults = request.POST['searchresults']
+        textbook = Textbook.objects.all().filter(booktitle__contains=searchresults) | Textbook.objects.all().filter(author__contains=searchresults) | Textbook.objects.all().filter(isbn__contains=searchresults)
+        
+        return render(request, 'catalogue/search.html', {'searchresults': textbook})
