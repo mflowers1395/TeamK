fba572dbda815e7681f7ec00a4a443325045317a
Marilyn Flowers <mflowers1@hawk.iit.edu>
2022-03-22 20:05:24 -0500

    Deletion

--- IPRO_webapp/settings.py
+++ IPRO_webapp/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+<<<<<<< HEAD
 
 import os
+=======
+import os #new
+>>>>>>> 2b5ad5b33008f401db220acb59a1494b5aafb956
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
