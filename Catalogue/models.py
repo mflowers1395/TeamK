cf45061db75ec8149508abacab075778e7a60a2a
Samruddhi <skakade2@hawk.iit.edu>
2022-03-29 21:00:17 -0500

    Added poster to catalogue

--- Catalogue/models.py
+++ Catalogue/models.py
@@ -4,6 +4,7 @@ class Textbook(models.Model):
     booktitle = models.CharField('Title', max_length=50)
     author = models.CharField('Author', max_length=30)
     isbn = models.CharField('ISBN', max_length=13)
+    poster = models.CharField('Poster', max_length=30)
 
     def __str__(self):
         return self.booktitle
