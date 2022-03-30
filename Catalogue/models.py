c9f9e14a84b5bd68fe5c0e3a90c55ede81c7702f
BoominHutJr <56770089+BoominHutJr@users.noreply.github.com>
2022-03-29 21:11:09 -0500

    fixed Catalogue/models.py not merging correctly

--- Catalogue/models.py
+++ Catalogue/models.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-
-class Textbook(models.Model):
-    booktitle = models.CharField('Title', max_length=50)
-    author = models.CharField('Author', max_length=30)
-    isbn = models.CharField('ISBN', max_length=13)
-
-    def __str__(self):
-        return self.booktitle
-=======
 from django.db import models
 
 class Textbook(models.Model):
@@ -19,4 +8,3 @@ class Textbook(models.Model):
 
     def __str__(self):
         return self.booktitle
->>>>>>> 911aaed20769ad750d24497adb73ca333f370dac
