ddc32c493ef7c5ee1907118a1484197624ff6b09
BoominHutJr <56770089+BoominHutJr@users.noreply.github.com>
2022-04-10 13:28:31 -0500

    added default price for textbook listings

--- Catalogue/models.py
+++ Catalogue/models.py
@@ -5,7 +5,7 @@ class Textbook(models.Model):
     booktitle = models.CharField('Title', max_length=50)
     author = models.CharField('Author', max_length=30)
     isbn = models.CharField('ISBN', max_length=13)
-    price = models.FloatField('Price', max_length=8)
+    price = models.FloatField('Price', max_length=8, default=10)
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
 
     def __str__(self):
