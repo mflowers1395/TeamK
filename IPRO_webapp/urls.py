17623d3d4d9df2229355dd16a42bced726d50ddb
BoominHutJr <56770089+BoominHutJr@users.noreply.github.com>
2022-04-19 18:08:49 -0500

    Removed landing page to prep for demo

--- IPRO_webapp/urls.py
+++ IPRO_webapp/urls.py
@@ -26,7 +26,7 @@ from Messaging_System import views as messaging_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', landing.launchLandingPage, name='landing'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
